c483692c61b7268c3226471f71e78af9df4774c0
Chuck <chuck.pitre@hotmail.com>
2024-05-04 23:57:29 -0400

    never give up; never surrender

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 from datetime import datetime
 import pytz
-import dotenv
+import sys
 import os
-strSSHKEY = str(os.getenv('env_sshkey'))
-strIP = str(os.getenv('env_ipv4'))
-
+from dotenv import load_dotenv
+from loguru import logger  # Loguru is a library for easier logging in Python
 
 app = Flask(__name__)
+DEBUG_MODE = os.getenv('DEBUG_MODE', 'False').lower() in ('true', '1', 't', 'y')
+# Set the logging level based on whether the application is in debug mode
+log_level = "DEBUG" if DEBUG_MODE else "INFO"
+# Remove all handlers associated with the logger
+logger.remove()
+# Re-add a standard output handler with the newly set log level
+logger.add(sys.stdout, level=log_level)
+logger.debug("***BEGIN app.py****")
 
 elections = {
     'federal': '2025-10-19',
